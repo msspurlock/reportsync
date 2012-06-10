ada514e80f772bd63c7ef6acd2f982c72a6ddc19
nunespascal <nunespascal@6183b42a-011c-2658-af83-f8c6c7ca6a2c>
2012-06-10 18:03:06 +0000

    Settings added to remember user selections Assembly info updated.

--- trunk/ReportSync.Designer.cs
+++ trunk/ReportSync.Designer.cs
@@ -29,18 +29,11 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReportSync));
-            this.txtSourceUrl = new System.Windows.Forms.TextBox();
             this.btnSourceLoad = new System.Windows.Forms.Button();
-            this.txtSourceUser = new System.Windows.Forms.TextBox();
-            this.txtSourcePassword = new System.Windows.Forms.TextBox();
             this.rptSourceTree = new System.Windows.Forms.TreeView();
             this.btnDownload = new System.Windows.Forms.Button();
             this.dlgDest = new System.Windows.Forms.FolderBrowserDialog();
-            this.txtLocalPath = new System.Windows.Forms.TextBox();
             this.btnDest = new System.Windows.Forms.Button();
-            this.txtDestUrl = new System.Windows.Forms.TextBox();
-            this.txtDestUser = new System.Windows.Forms.TextBox();
-            this.txtDestPassword = new System.Windows.Forms.TextBox();
             this.btnDestLoad = new System.Windows.Forms.Button();
             this.rptDestTree = new System.Windows.Forms.TreeView();
             this.btnSync = new System.Windows.Forms.Button();
@@ -48,22 +41,24 @@
             this.lblDestUser = new System.Windows.Forms.Label();
             this.lblDestUrl = new System.Windows.Forms.Label();
             this.grpSource = new System.Windows.Forms.GroupBox();
+            this.chkSaveSource = new System.Windows.Forms.CheckBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.lblSrcUrl = new System.Windows.Forms.Label();
             this.grpDest = new System.Windows.Forms.GroupBox();
+            this.chkSaveDest = new System.Windows.Forms.CheckBox();
             this.lblDest = new System.Windows.Forms.Label();
             this.btnUpload = new System.Windows.Forms.Button();
+            this.txtDestUrl = new System.Windows.Forms.TextBox();
+            this.txtDestUser = new System.Windows.Forms.TextBox();
+            this.txtDestPassword = new System.Windows.Forms.TextBox();
+            this.txtSourceUrl = new System.Windows.Forms.TextBox();
+            this.txtSourceUser = new System.Windows.Forms.TextBox();
+            this.txtSourcePassword = new System.Windows.Forms.TextBox();
+            this.txtLocalPath = new System.Windows.Forms.TextBox();
             this.grpSource.SuspendLayout();
             this.grpDest.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // txtSourceUrl
-            // 
-            this.txtSourceUrl.Location = new System.Drawing.Point(32, 16);
-            this.txtSourceUrl.Name = "txtSourceUrl";
-            this.txtSourceUrl.Size = new System.Drawing.Size(193, 20);
-            this.txtSourceUrl.TabIndex = 1;
             // 
             // btnSourceLoad
             // 
@@ -74,22 +69,6 @@
             this.btnSourceLoad.Text = "Load";
             this.btnSourceLoad.UseVisualStyleBackColor = true;
             this.btnSourceLoad.Click += new System.EventHandler(this.btnLoad_Click);
-            // 
-            // txtSourceUser
-            // 
-            this.txtSourceUser.Location = new System.Drawing.Point(64, 41);
-            this.txtSourceUser.Name = "txtSourceUser";
-            this.txtSourceUser.Size = new System.Drawing.Size(100, 20);
-            this.txtSourceUser.TabIndex = 2;
-            this.txtSourceUser.Text = "Administrator";
-            // 
-            // txtSourcePassword
-            // 
-            this.txtSourcePassword.Location = new System.Drawing.Point(64, 67);
-            this.txtSourcePassword.Name = "txtSourcePassword";
-            this.txtSourcePassword.PasswordChar = '*';
-            this.txtSourcePassword.Size = new System.Drawing.Size(100, 20);
-            this.txtSourcePassword.TabIndex = 3;
             // 
             // rptSourceTree
             // 
@@ -109,13 +88,6 @@
             this.btnDownload.UseVisualStyleBackColor = true;
             this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
             // 
-            // txtLocalPath
-            // 
-            this.txtLocalPath.Location = new System.Drawing.Point(66, 458);
-            this.txtLocalPath.Name = "txtLocalPath";
-            this.txtLocalPath.Size = new System.Drawing.Size(197, 20);
-            this.txtLocalPath.TabIndex = 5;
-            // 
             // btnDest
             // 
             this.btnDest.Location = new System.Drawing.Point(272, 458);
@@ -125,29 +97,6 @@
             this.btnDest.Text = "...";
             this.btnDest.UseVisualStyleBackColor = true;
             this.btnDest.Click += new System.EventHandler(this.btnDest_Click);
-            // 
-            // txtDestUrl
-            // 
-            this.txtDestUrl.Location = new System.Drawing.Point(27, 16);
-            this.txtDestUrl.Name = "txtDestUrl";
-            this.txtDestUrl.Size = new System.Drawing.Size(205, 20);
-            this.txtDestUrl.TabIndex = 8;
-            // 
-            // txtDestUser
-            // 
-            this.txtDestUser.Location = new System.Drawing.Point(65, 42);
-            this.txtDestUser.Name = "txtDestUser";
-            this.txtDestUser.Size = new System.Drawing.Size(100, 20);
-            this.txtDestUser.TabIndex = 9;
-            this.txtDestUser.Text = "Administrator";
-            // 
-            // txtDestPassword
-            // 
-            this.txtDestPassword.Location = new System.Drawing.Point(65, 67);
-            this.txtDestPassword.Name = "txtDestPassword";
-            this.txtDestPassword.PasswordChar = '*';
-            this.txtDestPassword.Size = new System.Drawing.Size(100, 20);
-            this.txtDestPassword.TabIndex = 10;
             // 
             // btnDestLoad
             // 
@@ -207,6 +156,7 @@
             // 
             // grpSource
             // 
+            this.grpSource.Controls.Add(this.chkSaveSource);
             this.grpSource.Controls.Add(this.label2);
             this.grpSource.Controls.Add(this.label1);
             this.grpSource.Controls.Add(this.lblSrcUrl);
@@ -221,6 +171,18 @@
             this.grpSource.TabIndex = 19;
             this.grpSource.TabStop = false;
             this.grpSource.Text = "Source SSRS web service";
+            // 
+            // chkSaveSource
+            // 
+            this.chkSaveSource.AutoSize = true;
+            this.chkSaveSource.Checked = true;
+            this.chkSaveSource.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkSaveSource.Location = new System.Drawing.Point(171, 44);
+            this.chkSaveSource.Name = "chkSaveSource";
+            this.chkSaveSource.Size = new System.Drawing.Size(51, 17);
+            this.chkSaveSource.TabIndex = 22;
+            this.chkSaveSource.Text = "Save";
+            this.chkSaveSource.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
@@ -251,6 +213,7 @@
             // 
             // grpDest
             // 
+            this.grpDest.Controls.Add(this.chkSaveDest);
             this.grpDest.Controls.Add(this.lblDestUrl);
             this.grpDest.Controls.Add(this.txtDestUrl);
             this.grpDest.Controls.Add(this.txtDestUser);
@@ -265,6 +228,18 @@
             this.grpDest.TabIndex = 20;
             this.grpDest.TabStop = false;
             this.grpDest.Text = "Destination SSRS web service";
+            // 
+            // chkSaveDest
+            // 
+            this.chkSaveDest.AutoSize = true;
+            this.chkSaveDest.Checked = true;
+            this.chkSaveDest.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkSaveDest.Location = new System.Drawing.Point(176, 44);
+            this.chkSaveDest.Name = "chkSaveDest";
+            this.chkSaveDest.Size = new System.Drawing.Size(51, 17);
+            this.chkSaveDest.TabIndex = 22;
+            this.chkSaveDest.Text = "Save";
+            this.chkSaveDest.UseVisualStyleBackColor = true;
             // 
             // lblDest
             // 
@@ -285,6 +260,71 @@
             this.btnUpload.UseVisualStyleBackColor = true;
             this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
             // 
+            // txtDestUrl
+            // 
+            this.txtDestUrl.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::ReportSync.Properties.Settings.Default, "DestUrl", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtDestUrl.Location = new System.Drawing.Point(27, 16);
+            this.txtDestUrl.Name = "txtDestUrl";
+            this.txtDestUrl.Size = new System.Drawing.Size(205, 20);
+            this.txtDestUrl.TabIndex = 8;
+            this.txtDestUrl.Text = global::ReportSync.Properties.Settings.Default.DestUrl;
+            // 
+            // txtDestUser
+            // 
+            this.txtDestUser.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::ReportSync.Properties.Settings.Default, "DestUser", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtDestUser.Location = new System.Drawing.Point(65, 42);
+            this.txtDestUser.Name = "txtDestUser";
+            this.txtDestUser.Size = new System.Drawing.Size(100, 20);
+            this.txtDestUser.TabIndex = 9;
+            this.txtDestUser.Text = global::ReportSync.Properties.Settings.Default.DestUser;
+            // 
+            // txtDestPassword
+            // 
+            this.txtDestPassword.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::ReportSync.Properties.Settings.Default, "DestPassword", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtDestPassword.Location = new System.Drawing.Point(65, 67);
+            this.txtDestPassword.Name = "txtDestPassword";
+            this.txtDestPassword.PasswordChar = '*';
+            this.txtDestPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtDestPassword.TabIndex = 10;
+            this.txtDestPassword.Text = global::ReportSync.Properties.Settings.Default.DestPassword;
+            // 
+            // txtSourceUrl
+            // 
+            this.txtSourceUrl.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::ReportSync.Properties.Settings.Default, "SourceUrl", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtSourceUrl.Location = new System.Drawing.Point(32, 16);
+            this.txtSourceUrl.Name = "txtSourceUrl";
+            this.txtSourceUrl.Size = new System.Drawing.Size(193, 20);
+            this.txtSourceUrl.TabIndex = 1;
+            this.txtSourceUrl.Text = global::ReportSync.Properties.Settings.Default.SourceUrl;
+            // 
+            // txtSourceUser
+            // 
+            this.txtSourceUser.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::ReportSync.Properties.Settings.Default, "SourceUser", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtSourceUser.Location = new System.Drawing.Point(64, 41);
+            this.txtSourceUser.Name = "txtSourceUser";
+            this.txtSourceUser.Size = new System.Drawing.Size(100, 20);
+            this.txtSourceUser.TabIndex = 2;
+            this.txtSourceUser.Text = global::ReportSync.Properties.Settings.Default.SourceUser;
+            // 
+            // txtSourcePassword
+            // 
+            this.txtSourcePassword.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::ReportSync.Properties.Settings.Default, "SourcePassword", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtSourcePassword.Location = new System.Drawing.Point(64, 67);
+            this.txtSourcePassword.Name = "txtSourcePassword";
+            this.txtSourcePassword.PasswordChar = '*';
+            this.txtSourcePassword.Size = new System.Drawing.Size(100, 20);
+            this.txtSourcePassword.TabIndex = 3;
+            this.txtSourcePassword.Text = global::ReportSync.Properties.Settings.Default.SourcePassword;
+            // 
+            // txtLocalPath
+            // 
+            this.txtLocalPath.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::ReportSync.Properties.Settings.Default, "LocalPath", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtLocalPath.Location = new System.Drawing.Point(66, 458);
+            this.txtLocalPath.Name = "txtLocalPath";
+            this.txtLocalPath.Size = new System.Drawing.Size(197, 20);
+            this.txtLocalPath.TabIndex = 5;
+            this.txtLocalPath.Text = global::ReportSync.Properties.Settings.Default.LocalPath;
+            // 
             // ReportSync
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -301,6 +341,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ReportSync";
             this.Text = "ReportSync";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ReportSync_FormClosed);
             this.grpSource.ResumeLayout(false);
             this.grpSource.PerformLayout();
             this.grpDest.ResumeLayout(false);
@@ -337,6 +378,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.CheckBox chkSaveSource;
+        private System.Windows.Forms.CheckBox chkSaveDest;
     }
 }
 
